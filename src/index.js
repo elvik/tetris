d091d73864746eddbd5d1ef835fb170a431c3db4
elvik☆ <122831026+elvik@users.noreply.github.com>
2023-01-17 22:43:22 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+let array2d = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
+
+let gameArea = document.getElementById("gameArea");
+
+// document.createElement("div")
+// col.className = "col";
+// row.appendChild(col);
+// gameArea.appendChild(row);
+// array[i][j]
+
+function draw(array) {
+  for (let i = 0; i < array.length; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    gameArea.appendChild(row);
+    for (let j = 0; j < array[i].length; j++) {
+      let col = document.createElement("div");
+      col.className = "col";
+      row.appendChild(col);
+    }
+  }
+}
+
+draw(array2d);
